d815ce9d992d5dcdc8f756a77ee2e8a72c6a4c73
endless-geriot <alisiageriot@gmail.com>
2025-05-30 14:15:31 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -2,4 +2,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PhoneBookTest {
+    @Test
+    void testAdd() {
+        PhoneBook book = new PhoneBook();
+        int count = book.add("Alice", "1111");
+        assertEquals(1, count);
+    }
+
+    @Test
+    void testAddMultipleEntries() {
+        PhoneBook book = new PhoneBook();
+        book.add("Alice", "111");
+        int count = book.add("Nastya", "222");
+        assertEquals(2, count);
+    }
 }
