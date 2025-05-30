54374a7d548820b666ca0a4c254a75b41676b7be
endless-geriot <alisiageriot@gmail.com>
2025-05-30 14:20:35 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, String> phoneToName = new HashMap<>();
+    private final Map<String, String> nameToPhone = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (nameToPhone.containsKey(name)) {
+            return nameToPhone.size(); // не добавляем дубликаты
+        }
+        nameToPhone.put(name, number);
+        phoneToName.put(number, name);
+        return nameToPhone.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -16,4 +16,11 @@ public class PhoneBookTest {
         int count = book.add("Nastya", "222");
         assertEquals(2, count);
     }
+    @Test
+    void testAddDupl() {
+        PhoneBook book = new PhoneBook();
+        book.add("Alice", "111");
+        int count = book.add("Alice", "111");
+        assertEquals(1, count);
+    }
 }
